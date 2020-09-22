5ad0fe97f38670091981ca3fba0210a68241d541
TMD <kk412027247@gmail.com>
2020-09-22 23:51:57 +0800

    RestTemplate get/post/put/delete

---
+++ src/main/java/tacos/restclient/RestExample.java
@@ -0,0 +1,28 @@
+package tacos.restclient;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.hateoas.MediaTypes;
+import org.springframework.hateoas.client.Traverson;
+import org.springframework.web.client.RestTemplate;
+
+import java.net.URI;
+
+@SpringBootConfiguration
+@Slf4j
+public class RestExample {
+    public RestExample() {
+    }
+
+    @Bean
+    public RestTemplate restTemplate(){
+        return new RestTemplate();
+    }
+
+    @Bean
+    public Traverson traverson(){
+        return new Traverson(URI.create("hattp://localhost:8080/api"), MediaTypes.HAL_JSON);
+    }
+
+}
---
+++ src/main/java/tacos/restclient/TacoCloudClient.java
@@ -0,0 +1,52 @@
+package tacos.restclient;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.hateoas.client.Traverson;
+import org.springframework.http.HttpMethod;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+import tacos.Ingredient;
+
+import java.util.List;
+
+
+@Service
+@Slf4j
+public class TacoCloudClient {
+
+
+    private RestTemplate rest;
+    private Traverson traverson;
+
+    @Autowired
+    public TacoCloudClient(RestTemplate rest, Traverson traverson) {
+        this.rest = rest;
+        this.traverson = traverson;
+    }
+
+
+    public Ingredient getIngredientById(String ingredientId){
+        return rest.getForObject("http://localhost:8080/ingredients/{id}",
+                Ingredient.class, ingredientId);
+    }
+
+    public List<Ingredient> getAllIngredients(){
+        return rest.exchange("http://localhost:8080/ingredients",
+                HttpMethod.GET, null, new ParameterizedTypeReference<List<Ingredient>>() {}).getBody();
+    }
+
+
+    public void updateIngredient(Ingredient ingredient){
+        rest.put("http://localhost:8080/ingredients/{id}",ingredient, ingredient.getId());
+    }
+
+    public void deleteIngredient(Ingredient ingredient){
+        rest.delete("http://localhost:8080/ingredients/{id}",ingredient.getId());
+    }
+
+    public Ingredient createIngredient(Ingredient ingredient){
+        return rest.postForObject("http://localhost:8080/ingredients",ingredient, Ingredient.class);
+    }
+}
